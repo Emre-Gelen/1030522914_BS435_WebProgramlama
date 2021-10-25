4876f405dc8231a7c1f7272f4ade7fa92f47b32c
Emre-Gelen <emregeln@gmail.com>
2021-10-25 21:05:26 +0300

    First Commit

---
+++ odev1/kod.js
@@ -0,0 +1,29 @@
+const trueCard = getRandomCard();
+let hak = 3;
+let gameOver = false;
+function getRandomCard() {
+    return Math.floor(Math.random() * 3);
+}
+
+function btnOnClick(comp) {
+    var selectedId = comp.id.match(/\d+$/)[0]
+    if (!gameOver) {
+        if (selectedId == trueCard && hak > 1) {
+            comp.src = '../Odev1/Images/cat.jpg'
+            document.getElementById('kazandiId').style.display = 'unset'
+            gameOver=true;
+        }
+        else if (hak > 1) {
+            comp.src = '../Odev1/Images/dog.jpg'
+            hak -= 1;
+        }
+        else if (hak == 1) {
+            document.getElementById('yenildiId').style.display = 'unset'
+            gameOver=true;
+        }
+    }
+}
+
+function resetGameClick() {
+    location.reload();
+}
